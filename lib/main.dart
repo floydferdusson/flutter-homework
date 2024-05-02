a19e5ed150a1156017cda472108368a14b8cc74f
floydferdusson <floydferdusson@proton.me>
2024-05-03 07:47:07 +0800

    Update main.dart
    delete unused import

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:simple_login/ui/password_textfield.dart';
 import 'package:simple_login/ui/screen.dart';
