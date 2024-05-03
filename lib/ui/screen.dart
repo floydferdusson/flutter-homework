331c416102bc38e1e935fcb3f81fcc329a727a47
floydferdusson <floydferdusson@proton.me>
2024-05-03 15:18:57 +0800

    components pt.ii

--- lib/ui/screen.dart
+++ lib/ui/screen.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
-class Screen extends StatelessWidget {
+class ScreenF extends StatelessWidget {
   final Widget child;
   final String imgPath;
 
-  const Screen({
+  const ScreenF({
     super.key,
     required this.child,
     required this.imgPath,
